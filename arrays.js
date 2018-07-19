50802dfc4a4409f82ad688a9cf75b8fe8ccd05d8
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-19 04:05:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var array = [1, 2, 3];
-  array.unshift(0);
-
-  element = array;
-
+  var element = [1, 2, 3];
+  element.unshift(0);
   return element;
 
 }
