90896fa16f70e92af39d3b0f976a2bc90742d0bd
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-19 03:54:28 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var array = [1, 2, 3];
+  array.push(0);
+
+  element = array;
+
+  return element;
+
+}
+
+//function destructivelyAddElementToBeginningOfArray(){}
