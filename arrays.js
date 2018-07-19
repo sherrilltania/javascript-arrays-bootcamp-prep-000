9ac1224298d308ec3eecf514e04de6b752a8bb15
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-19 04:14:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element){
+  return [array, ...element]
+}
