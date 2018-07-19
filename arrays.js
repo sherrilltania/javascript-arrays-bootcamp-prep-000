447e7e1dceb39b7581a2c31c4cc250502c77f6bf
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-19 04:51:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,8 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array){
